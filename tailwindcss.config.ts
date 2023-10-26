7ca8a9954c6ddb100b95f7b47a30f4d0c4f07b4b
Faarlv <rhfa.fds@gmail.com>
2023-10-26 12:24:13 +0700

    add products menu

--- tailwindcss.config.ts
+++ tailwindcss.config.ts
@@ -2,6 +2,19 @@ import type { Config } from 'tailwindcss'
 
 export default <Partial<Config>>{
  theme: {
-     extend: {},
-    }
+     extend: {
+        colors: {
+            primary: "#6867C5",
+    },
+ },
+ container: {
+    screens: {
+        md: "598px",
+        sm: "470px",
+        xl: "1110px",
+        lg: "854px",
+    },
+    center: true,
+    },
+  }
 }
